5538e2185312f67b7ea30b8d5db66d7a600428c8
Genady Mironenko <genichm@gmail.com>
2021-04-06 22:14:09 +0300

    Add cpu metric command

--- Mnemox.Machine.Metrics/Linux/LinuxCpuMetrics.cs
+++ Mnemox.Machine.Metrics/Linux/LinuxCpuMetrics.cs
@@ -4,6 +4,8 @@ namespace Mnemox.Machine.Metrics.Linux
 {
     public class LinuxCpuMetrics : ICpuMetrics
     {
+        private const string COMMAND_TO_GET_CPU_USAGE = "top -b -n2 -p 1 | fgrep \"Cpu(s)\" | tail -1 | awk -F'id,' -v prefix=\"$prefix\" '{ split($1, vs, \",\"); v=vs[length(vs)]; sub(\"%\", \"\", v); printf \"%s%.1f%%\n\", prefix, 100 - v }'";
+        
         public float GetCpuUsagePercentage()
         {
             throw new System.NotImplementedException();
