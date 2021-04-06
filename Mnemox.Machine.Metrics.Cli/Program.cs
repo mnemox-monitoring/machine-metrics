00eb5d9721e6f3f5fe87bb8fdb8382e11902a700
Genady Mironenko <genichm@gmail.com>
2021-04-06 16:32:29 +0300

    Add cli

---
+++ Mnemox.Machine.Metrics.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mnemox.Machine.Metrics.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
